cd804e0526ca15184b52651391d2c91119f5988d
oculairmedia <emanuvaderland@gmail.com>
2026-08-18 12:49:26 -0400

    refactor(session): clean up unused imports in split session test suites

--- android-compose/core/data/src/test/java/com/letta/mobile/data/session/SessionScopedAdminRepositoriesTest.kt
+++ android-compose/core/data/src/test/java/com/letta/mobile/data/session/SessionScopedAdminRepositoriesTest.kt
@@ -1,11 +1,5 @@
 package com.letta.mobile.data.session
 
-import com.letta.mobile.data.api.LettaApiClient
-import com.letta.mobile.data.local.AgentDao
-import com.letta.mobile.data.local.AgentEntity
-import com.letta.mobile.data.local.ConversationDao
-import com.letta.mobile.data.local.ConversationEntity
-import com.letta.mobile.data.local.ConversationRefreshEntity
 import com.letta.mobile.data.model.AgentId
 import com.letta.mobile.data.model.Folder
 import com.letta.mobile.data.model.FolderId
@@ -13,38 +7,14 @@ import com.letta.mobile.data.model.Group
 import com.letta.mobile.data.model.GroupId
 import com.letta.mobile.data.model.Identity
 import com.letta.mobile.data.model.IdentityId
-import com.letta.mobile.data.model.LettaConfig
 import com.letta.mobile.data.model.Provider
 import com.letta.mobile.data.model.ProviderId
-import com.letta.mobile.testutil.FakeAgentApi
-import com.letta.mobile.testutil.FakeArchiveApi
-import com.letta.mobile.testutil.FakeConversationApi
 import com.letta.mobile.testutil.FakeFolderApi
 import com.letta.mobile.testutil.FakeGroupApi
 import com.letta.mobile.testutil.FakeIdentityApi
-import com.letta.mobile.testutil.FakeJobApi
-import com.letta.mobile.testutil.FakeMcpServerApi
-import com.letta.mobile.testutil.FakeModelApi
-import com.letta.mobile.testutil.FakePassageApi
-import com.letta.mobile.testutil.FakeProjectApi
-import com.letta.mobile.testutil.FakeProjectWorkApi
 import com.letta.mobile.testutil.FakeProviderApi
-import com.letta.mobile.testutil.FakeRunApi
-import com.letta.mobile.testutil.FakeScheduleApi
-import com.letta.mobile.testutil.FakeSettingsRepository
-import com.letta.mobile.testutil.FakeStepApi
-import com.letta.mobile.testutil.FakeToolApi
-import io.mockk.mockk
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
--- android-compose/core/data/src/test/java/com/letta/mobile/data/session/SessionScopedExecutionRepositoriesTest.kt
+++ android-compose/core/data/src/test/java/com/letta/mobile/data/session/SessionScopedExecutionRepositoriesTest.kt
@@ -1,47 +1,12 @@
 package com.letta.mobile.data.session
 
-import com.letta.mobile.data.api.LettaApiClient
-import com.letta.mobile.data.local.AgentDao
-import com.letta.mobile.data.local.AgentEntity
-import com.letta.mobile.data.local.ConversationDao
-import com.letta.mobile.data.local.ConversationEntity
-import com.letta.mobile.data.local.ConversationRefreshEntity
 import com.letta.mobile.data.model.Job
-import com.letta.mobile.data.model.JobListParams
-import com.letta.mobile.data.model.LettaConfig
 import com.letta.mobile.data.model.Run
-import com.letta.mobile.data.model.RunListParams
-import com.letta.mobile.data.model.RunRequestConfig
-import com.letta.mobile.data.model.StepListParams
-import com.letta.mobile.testutil.FakeAgentApi
-import com.letta.mobile.testutil.FakeArchiveApi
-import com.letta.mobile.testutil.FakeConversationApi
-import com.letta.mobile.testutil.FakeFolderApi
-import com.letta.mobile.testutil.FakeGroupApi
-import com.letta.mobile.testutil.FakeIdentityApi
 import com.letta.mobile.testutil.FakeJobApi
-import com.letta.mobile.testutil.FakeMcpServerApi
-import com.letta.mobile.testutil.FakeModelApi
-import com.letta.mobile.testutil.FakePassageApi
-import com.letta.mobile.testutil.FakeProjectApi
-import com.letta.mobile.testutil.FakeProjectWorkApi
-import com.letta.mobile.testutil.FakeProviderApi
 import com.letta.mobile.testutil.FakeRunApi
-import com.letta.mobile.testutil.FakeScheduleApi
-import com.letta.mobile.testutil.FakeSettingsRepository
 import com.letta.mobile.testutil.FakeStepApi
-import com.letta.mobile.testutil.FakeToolApi
-import io.mockk.mockk
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
